bcd2c2b8d944dd3255b508c07927d5f4b633e355
Sergey_Sharay <sergeysharay1987@gmail.com>
2021-06-07 20:19:32 +0300

    Adding '/' in path

--- bitrix24/urls.py
+++ bitrix24/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin', admin.site.urls),
-    path('lead', include('create_lead.urls'))
+    path('lead/', include('create_lead.urls'))
 ]
