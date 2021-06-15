9c4a7981ccbb33b4cf4dfe9a0a8becdeacf5894d
Sergey_Sharay <sergeysharay1987@gmail.com>
2021-06-15 18:54:02 +0300

    Some changes

--- create_lead/views.py
+++ create_lead/views.py
@@ -3,36 +3,47 @@ from .forms import CreateLead
 from fast_bitrix24 import Bitrix
 from dadata import Dadata
 
-token = "153e032b2e5fc2f0283305f6bcd1bd05364c1b8a"
-secret = "967b17ecea33bb4ccd6bfdc62c081b56010c18c0"
+token = "68de34222a74a59793cfd9d431a771fd79816581"
+secret = "f3da9bf600d9b2386a43d7146fab10d88f83f222"
 dadata = Dadata(token, secret)
 API_URL = "https://dadata.ru/api/clean/name/"
 URL_WEB_HOOK_TO_CALL_REST_API = 'https://b24-tc3mws.bitrix24.ru/rest/1/27b28um0db36ab7r'
 bx24 = Bitrix(URL_WEB_HOOK_TO_CALL_REST_API)
 
 
-def make_data_to_call_rest_api(fio, telephone, address):
-    fio = dadata.clean('name', fio)
-    telephone = dadata.clean('phone', telephone)
-    address = dadata.clean('address', address)
-    address_by_fias = dadata.find_by_id('address', address['fias_id'])
-    return fio, telephone, address_by_fias[0]['unrestricted_value']
+def check_empty_key_values(fio, telephone, address):
+    for dictionary in fio, telephone, address:
+        for key in dictionary:
+            if not dictionary[key]:
+                dictionary[key] = ''
+    print(fio, telephone, address, )
+    return fio, telephone, address
+
+
+def make_data_to_call_rest_api(fio_from_form, telephone_from_form, address_from_form):
+    fio_from_form = dadata.clean('name', fio_from_form)
+    telephone_from_form = dadata.clean('phone', telephone_from_form)
+    address_from_form = dadata.clean('address', address_from_form)
+    address_from_form__by_fias = dadata.find_by_id('address', address_from_form['fias_id'])
+    fio, telephone, address = check_empty_key_values(fio_from_form, telephone_from_form, address_from_form__by_fias[0])
+    print(fio, telephone, address)
+    return fio, telephone, address
 
 
 def call_web_hook(fio_from_dadata, telephone_from_dadata, address_from_dadata_by_fias):
-    print(telephone_from_dadata)
+    # print(fio_from_dadata, telephone_from_dadata, telephone_from_dadata, sep = '\n')
     return bx24.call('crm.lead.add', items={
-                'fields':
-                    {
-                        "NAME": fio_from_dadata['name'],
-                        "SECOND_NAME": fio_from_dadata['patronymic'],
-                        "LAST_NAME": fio_from_dadata['surname'],
-                        "ADDRESS": address_from_dadata_by_fias,
-                        # "ADDRESS": address_from_dadata["result"],
-                        "PHONE": [
-                            {"VALUE": telephone_from_dadata['phone'], "VALUE_TYPE": telephone_from_dadata['type']}]
-                    }
-            })
+        'fields':
+            {
+                "NAME": fio_from_dadata['name'],
+                "SECOND_NAME": fio_from_dadata['patronymic'],
+                "LAST_NAME": fio_from_dadata['surname'],
+                "ADDRESS": address_from_dadata_by_fias['unrestricted_value'],
+                # "ADDRESS": address_from_dadata["result"],
+                "PHONE": [
+                    {"VALUE": telephone_from_dadata['phone'], "VALUE_TYPE": telephone_from_dadata['type']}]
+            }
+    })
 
 
 def hello_user(request):
@@ -53,7 +64,12 @@ def create_lead(request):
                 bound_form.cleaned_data['fio'],
                 bound_form.cleaned_data['telephone'],
                 bound_form.cleaned_data['address_of_lead'])
-            call_web_hook(fio_from_dadata, telephone_from_dadata, address_from_dadata_by_fias)
+            fio, telephone, address = check_empty_key_values(fio_from_dadata, telephone_from_dadata,
+                                                             address_from_dadata_by_fias)
+            #print(fio, telephone, address, sep = '\n')
+            #print()
+            #call_web_hook(fio_from_dadata, telephone_from_dadata, address_from_dadata_by_fias)
+            call_web_hook(fio, telephone, address)
             return redirect('https://b24-tc3mws.bitrix24.ru/stream/')
         else:
             form = CreateLead(request.POST)
